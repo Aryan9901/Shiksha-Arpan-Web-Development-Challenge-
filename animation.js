372e956c6545f0565fdae14c09c1752320920c14
Aryan9901 <911aaryan@gmail.com>
2023-07-23 19:57:34 +0530

    added course section

--- animation.js
+++ animation.js
@@ -44,3 +44,84 @@ tl.from(".main__heading p", {
 		},
 		"hii"
 	);
+// image pineed scroll timeline
+// gsap.to(".courses__details", {
+// 	gap: "1rem",
+// 	stagger: 4,
+// 	duration: 4,
+// 	delay: 2,
+// 	ease: Expo.easeInOut,
+// 	scrollTrigger: {
+// 		trigger: ".courses__details",
+// 		scroller: "body",
+// 		start: "top 0%",
+// 		end: "bottom top",
+// 		// scrollerStart: "top bottom",
+// 		markers: true,
+// 		scrub: 2,
+// 	},
+// });
+
+gsap.utils.toArray(".courses__details").forEach((element) => {
+	gsap.to(element, {
+		gap: "3rem",
+		duration: 4,
+		delay: 1,
+		ease: "expo.inOut",
+		scrollTrigger: {
+			trigger: element,
+			scroller: "body",
+			start: "top center", // Adjust the start and end values to control when the animation starts and ends
+			end: "+=30", // Adjust the start and end values to control when the animation starts and ends
+			// scrollerStart: "top bottom",
+			// markers: true,
+			scrub: 2,
+		},
+	});
+});
+
+// image scroll
+
+// gsap.to(".courses__img", {
+// 	scrollTrigger: {
+// 		trigger: ".courses__img top",
+// 		scroller: ".courses__img",
+// 		start: "top top",
+// 		end: "bottom bottom",
+// 		scrollerStart: "top center",
+// 		pin: true,
+// 		markers: true,
+// 	},
+// });
+
+var g = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".courses__container",
+		scroller: "body",
+		start: "top -120%",
+		end: "top -400%",
+		// scrollerStart: "top center",
+		// markers: true,
+		scrub: 2,
+	},
+});
+g.to("body", {
+	duration: 4,
+	backgroundColor: "#C8E8FC",
+	ease: Expo.power2,
+})
+	.to("body", {
+		duration: 5,
+		backgroundColor: "#ff919191",
+		ease: Expo.power2,
+	})
+	.to("body", {
+		duration: 2,
+		backgroundColor: "#fcfffc",
+		ease: Expo.power2,
+	})
+	.to("body", {
+		duration: 2,
+		backgroundColor: "#c8e8fc",
+		ease: Expo.power2,
+	});
